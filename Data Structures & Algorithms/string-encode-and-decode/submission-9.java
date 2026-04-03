32532d3854dfffcb2d347fecd85301cb5787e5ea
Raditya0902 <124700496+Raditya0902@users.noreply.github.com>
2026-04-03 15:25:52 -0700

    Add: string-encode-and-decode - submission-9

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-9.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for(String str: strs){
+            sb.append(str.length()).append("#").append(str);
+        }
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> res = new ArrayList<>();
+        if(str.equals("")) return res;
+        int i = 0, j = 0;
+        while(j < str.length()){
+            i = j;
+            while(str.charAt(j) != '#'){
+                j++;
+            }
+            int len = Integer.parseInt(str.substring(i,j));
+            i = j + 1;
+            j = j + len + 1;
+            res.add(str.substring(i,j));
+        }
+         return res;
+    }
+}
