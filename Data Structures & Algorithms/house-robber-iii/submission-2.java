a5e6738c49662288ea4277ebcaba5d530f867b1c
Raditya0902 <124700496+Raditya0902@users.noreply.github.com>
2026-04-08 18:50:29 -0700

    Add: house-robber-iii - submission-2

---
+++ Data Structures & Algorithms/house-robber-iii/submission-2.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    HashMap<TreeNode, Integer> map = new HashMap<>();
+    public int rob(TreeNode root) {
+        map.put(null, 0);
+        return dfs(root);
+    }
+
+    int dfs(TreeNode node){
+        if(map.containsKey(node)) return map.get(node);
+        int res = node.val;
+        if(node.left != null){
+            res += dfs(node.left.left) + dfs(node.left.right);
+        }
+        if(node.right != null){
+            res += dfs(node.right.left) + dfs(node.right.right);
+        }
+        res = Math.max(res, dfs(node.left) + dfs(node.right));
+        map.put(node, res);
+        return res;
+    }
+}
