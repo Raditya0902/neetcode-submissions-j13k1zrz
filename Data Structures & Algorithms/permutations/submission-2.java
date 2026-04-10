83c4c6c4a1f5362bedc5c124010a6518620275dd
Raditya0902 <124700496+Raditya0902@users.noreply.github.com>
2026-04-10 00:39:07 -0700

    Add: permutations - submission-2

---
+++ Data Structures & Algorithms/permutations/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> permute(int[] nums) {
+        backtrack(0, nums, new ArrayList<>(), new boolean[nums.length]);
+        return res;
+    }
+
+    void backtrack(int idx, int[] nums, List<Integer> ls, boolean[] pick){
+        if(ls.size() == nums.length){
+            res.add(new ArrayList<>(ls));
+            return;
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            if(pick[i]) continue;
+            ls.add(nums[i]);
+            pick[i] = true;
+            backtrack(i+1, nums, ls, pick);
+            ls.remove(ls.size() - 1);
+            pick[i] = false;
+        }
+    }
+}
