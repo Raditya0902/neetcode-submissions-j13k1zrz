11e6a5bb90c90025b4e84c80e3f5dcb48e3ca9fa
Raditya0902 <124700496+Raditya0902@users.noreply.github.com>
2026-04-27 21:18:47 -0700

    Add: permutation-string - submission-9

---
+++ Data Structures & Algorithms/permutation-string/submission-9.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s1.length(), m = s2.length();
+        if(n > m) return false;
+        int[] c1 = new int[26];
+        int[] c2 = new int[26];
+        int matches = 0;
+
+        for(int i = 0; i < n; i++){
+            c1[s1.charAt(i) - 'a']++;
+            c2[s2.charAt(i) - 'a']++;
+        }
+
+        for(int i = 0; i < 26; i++){
+            if(c1[i] == c2[i]) matches++;
+        }
+
+        int l = 0;
+
+        for(int r = n; r < m; r++){
+            if(matches == 26) return true;
+            c2[s2.charAt(r) - 'a']++;
+            if(c1[s2.charAt(r) - 'a'] == c2[s2.charAt(r) - 'a']) matches++;
+            else if(c1[s2.charAt(r) - 'a'] + 1 == c2[s2.charAt(r) - 'a']) matches--;
+            c2[s2.charAt(l) - 'a']--;
+            if(c1[s2.charAt(l)- 'a'] == c2[s2.charAt(l) - 'a']) matches++;
+            else if(c1[s2.charAt(l) - 'a']  == c2[s2.charAt(l) - 'a'] + 1) matches--;
+            l++;
+        }
+        return matches == 26;
+    }
+}
