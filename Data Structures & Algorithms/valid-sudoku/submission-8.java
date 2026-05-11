9dd14b1e4fef42c1ea871a7e8215256e00fd404f
Raditya0902 <124700496+Raditya0902@users.noreply.github.com>
2026-05-11 11:03:54 -0700

    Add: valid-sudoku - submission-8

---
+++ Data Structures & Algorithms/valid-sudoku/submission-8.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Map<Integer, Set<Character>> row = new HashMap<>();
+        Map<Integer, Set<Character>> col = new HashMap<>();
+        Map<String, Set<Character>> box = new HashMap<>();
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] == '.') continue;
+                row.putIfAbsent(i, new HashSet<>());
+                col.putIfAbsent(j, new HashSet<>());
+                box.putIfAbsent(i/3 + "," + j/3, new HashSet<>());
+                if(row.get(i).contains(board[i][j]) || 
+                    col.get(j).contains(board[i][j]) || 
+                    box.get(i/3 + "," + j/3).contains(board[i][j]))
+                {
+                    return false;
+                }
+                row.get(i).add(board[i][j]);
+                col.get(j).add(board[i][j]);
+                box.get(i/3 +  "," +j/3).add(board[i][j]);
+            }
+        }
+        return true;
+    }
+}
