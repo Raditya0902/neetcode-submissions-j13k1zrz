59b3c9172fef627233a263ca49f7e26eb4f25c9c
Aditya Rallapalli <124700496+Raditya0902@users.noreply.github.com>
2026-06-04 19:10:34 -0700

    Add: pow-x-n - submission-2

---
+++ Data Structures & Algorithms/pow-x-n/submission-2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n == 0) return 1;
+        if(x == 0) return 0;
+        double res = helper(x, Math.abs(n));
+        return n >= 0 ? res : 1/res;
+    }
+
+    double helper(double x, int n){
+        if(n == 0) return 1;
+        double half = helper(x, n/2);
+        return (n % 2 == 0) ? half * half : x * half * half;
+    }
+}
