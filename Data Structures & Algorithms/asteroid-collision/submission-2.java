ebbf8857a31177db9b00257306e951c5a16c97b7
Aditya Rallapalli <124700496+Raditya0902@users.noreply.github.com>
2026-06-12 13:30:56 -0700

    Add: asteroid-collision - submission-2

---
+++ Data Structures & Algorithms/asteroid-collision/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> st = new Stack<>();
+        for(int a: asteroids){
+            boolean destroyed = false;
+            while(!st.isEmpty() && st.peek() > 0 && a < 0){
+                int top = st.peek();
+                if(top < -a){
+                    st.pop();
+                    continue;
+                }else if(top == -a){
+                    st.pop();
+                    destroyed = true;
+                    break;
+                }else{
+                    destroyed = true;
+                    break;
+                }
+            }
+            if(!destroyed) st.push(a);
+        }
+        int[] res = new int[st.size()];
+        for(int i = st.size() - 1; i >= 0; i--){
+            res[i] = st.pop();
+        }
+        return res;
+    }
+}
