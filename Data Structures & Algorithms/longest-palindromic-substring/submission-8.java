be86fd9da673bf789c699657e2b8c61da0a3f236
Aditya Rallapalli <124700496+Raditya0902@users.noreply.github.com>
2026-06-13 16:03:02 -0700

    Add: longest-palindromic-substring - submission-8

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-8.java
@@ -0,0 +1,24 @@
+class Solution {
+    Boolean[][] memo;
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        memo = new Boolean[n][n];
+        int start = 0, maxLen = 1;
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                if(isPalin(s, i, j) && j - i + 1 > maxLen){
+                    maxLen = j - i + 1;
+                    start = i;
+                }
+            }
+        }
+        return s.substring(start, start + maxLen);
+    }
+
+    boolean isPalin(String s, int i, int j){
+        if(i >= j) return true;
+        if(memo[i][j] != null) return memo[i][j];
+        if(s.charAt(i) == s.charAt(j)) return memo[i][j] = isPalin(s, i + 1, j - 1);
+        return memo[i][j] = false;
+    }
+}
