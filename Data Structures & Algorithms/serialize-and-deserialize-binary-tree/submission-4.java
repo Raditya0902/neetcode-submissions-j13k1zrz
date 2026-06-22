620e85e1f54226b52cdd4433d95eccdca06c427f
Aditya Rallapalli <124700496+Raditya0902@users.noreply.github.com>
2026-06-21 20:22:00 -0700

    Add: serialize-and-deserialize-binary-tree - submission-4

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-4.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Codec {
+
+    // Encodes a tree to a single string.
+    int idx = 0;
+    List<String> strs = new ArrayList<>();
+    public String serialize(TreeNode root) {
+        dfs(root);
+        return String.join(",", strs);
+    }
+
+    void dfs(TreeNode node){
+        if(node == null){
+            strs.add("N");
+            return;
+        }
+        strs.add(String.valueOf(node.val));
+        dfs(node.left);
+        dfs(node.right);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] ls = data.split(",");
+        return dfsD(ls);
+    }
+
+    TreeNode dfsD(String[] ls){
+        if(ls[idx].equals("N")){
+            idx++;
+            return null;
+        }
+        TreeNode node = new TreeNode(Integer.parseInt(ls[idx]));
+        idx++;
+        node.left = dfsD(ls);
+        node.right = dfsD(ls);
+        return node; 
+    }
+}
