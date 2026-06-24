886d84fa4fedc8e8effd7b001bf9f8e19c756b87
Aditya Rallapalli <124700496+Raditya0902@users.noreply.github.com>
2026-06-23 19:14:44 -0700

    Add: minimum-array-end - submission-9

---
+++ Data Structures & Algorithms/minimum-array-end/submission-9.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long minEnd(int n, int x) {
+        long ans = x;
+        long rem = n - 1;
+        long bitInAns = 1;
+        
+        while(rem > 0){
+            if((x & bitInAns) == 0){
+                if(((rem & 1) == 1)){
+                    ans = ans | bitInAns;
+                }
+                rem >>= 1;
+            }
+            bitInAns <<= 1;
+        }
+        return ans;
+    }
+}
