ece29b63c37fbb2e2b6b25d4230ef71ef6eaa596
Aditya Rallapalli <124700496+Raditya0902@users.noreply.github.com>
2026-06-28 09:45:39 -0700

    Add: is-anagram - submission-20

---
+++ Data Structures & Algorithms/is-anagram/submission-20.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[]f = new int[26];
+        for(char ch: s.toCharArray()) f[ch-'a']++;
+        for(char ch: t.toCharArray()) f[ch-'a']--;
+        for(int a: f) if(a != 0) return false;
+        return true;
+    }
+}
