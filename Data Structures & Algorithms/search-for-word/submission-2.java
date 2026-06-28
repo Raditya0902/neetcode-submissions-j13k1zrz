2583f9b62a402bdf0056d78c5facf3a861ec15c8
Aditya Rallapalli <124700496+Raditya0902@users.noreply.github.com>
2026-06-28 13:54:08 -0700

    Add: search-for-word - submission-2

---
+++ Data Structures & Algorithms/search-for-word/submission-2.java
@@ -0,0 +1,24 @@
+class Solution {
+    int n;
+    int m;
+    public boolean exist(char[][] board, String word) {
+        this.n = board.length;
+        this.m = board[0].length;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(dfs(i,j,0,board,word)) return true;
+            }
+        }
+        return false;
+    }
+
+    boolean dfs(int i, int j, int idx, char[][] board, String word){
+        if(idx == word.length()) return true;
+        if(i < 0 || j < 0 || i >= n || j >= m || board[i][j] == '#' || board[i][j] != word.charAt(idx)) return false;
+        board[i][j] = '#';
+        boolean res = dfs(i + 1, j, idx + 1, board, word) || dfs(i-1, j, idx + 1, board, word) || dfs(i, j+1, idx+1, board, word) || dfs(i, j-1, idx+1, board, word);
+        board[i][j] = word.charAt(idx);
+        return res;
+    }
+}
