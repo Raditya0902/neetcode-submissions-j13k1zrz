250344557faf0af6776b7a03b7fd73b298be84ab
Aditya Rallapalli <124700496+Raditya0902@users.noreply.github.com>
2026-06-28 19:01:17 -0700

    Add: sum-of-all-subset-xor-totals - submission-8

---
+++ Data Structures & Algorithms/sum-of-all-subset-xor-totals/submission-8.java
@@ -0,0 +1,16 @@
+class Solution {
+    int[] sum = {0};
+    public int subsetXORSum(int[] nums) {
+        dfs(0, nums, 0);
+        return sum[0];
+    }
+
+    void dfs(int idx, int[] nums, int xor){
+        sum[0] += xor;
+        for(int i = idx; i < nums.length; i++){
+            xor ^= nums[i];
+            dfs(i+1, nums, xor);
+            xor ^= nums[i];
+        }
+    }
+}
