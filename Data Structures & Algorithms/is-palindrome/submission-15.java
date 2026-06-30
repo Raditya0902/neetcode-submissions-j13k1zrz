b0d3535d1d41dfcb7865aac52d484f8c25cc4ac1
Aditya Rallapalli <124700496+Raditya0902@users.noreply.github.com>
2026-06-29 23:41:25 -0700

    Add: is-palindrome - submission-15

---
+++ Data Structures & Algorithms/is-palindrome/submission-15.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+        while(l < r){
+            while(l < r && !isAlphaNum(s.charAt(l))) l++;
+            while(l < r && !isAlphaNum(s.charAt(r))) r--;
+            if(Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    boolean isAlphaNum(char ch){
+        if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) return true;
+        return false;
+    }
+}
