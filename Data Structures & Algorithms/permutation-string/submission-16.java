a19cdd99f7fa5bb85b81d111b6e2f0a54414599b
Aditya Rallapalli <124700496+Raditya0902@users.noreply.github.com>
2026-08-25 13:24:17 -0700

    Add: permutation-string - submission-16

---
+++ Data Structures & Algorithms/permutation-string/submission-16.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s1.length(), m = s2.length();
+        if(n > m) return false;
+
+        int[] c1 = new int[26];
+        int[] c2 = new int[26];
+
+        for(int i = 0; i < n; i++) {
+            c1[s1.charAt(i) - 'a']++;
+            c2[s2.charAt(i) - 'a']++;
+        }
+
+        int matches = 0;
+        for(int i = 0; i < 26; i++) if(c1[i] == c2[i]) matches++;
+        
+        int l = 0;
+        for(int i = n; i < m; i++){
+            if(matches == 26) return true;
+            char rc = s2.charAt(i);
+            c2[rc - 'a']++;
+            if(c1[rc-'a'] == c2[rc-'a']) matches++;
+            else if(c1[rc-'a'] + 1 == c2[rc - 'a']) matches--;
+            char lc = s2.charAt(l);
+            c2[lc-'a']--;
+            if(c1[lc-'a'] == c2[lc-'a']) matches++;
+            else if(c1[lc - 'a'] - 1 == c2[lc - 'a']) matches--;
+            l++;
+        }
+        return matches == 26 ? true : false;
+    }
+}
