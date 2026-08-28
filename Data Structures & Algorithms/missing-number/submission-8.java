62ce2eae6e10cbf460a2603b5d7009cfcfc4d389
Aditya Rallapalli <124700496+Raditya0902@users.noreply.github.com>
2026-08-28 10:51:49 -0700

    Add: missing-number - submission-8

---
+++ Data Structures & Algorithms/missing-number/submission-8.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor = 0;
+        for(int n: nums) xor ^= n;
+        for(int i = 0; i <= nums.length; i++) xor ^= i;
+        return xor;
+    }
+}
