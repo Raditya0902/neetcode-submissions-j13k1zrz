8aa4639be327d53a0499191334d7695615bb014c
Aditya Rallapalli <124700496+Raditya0902@users.noreply.github.com>
2026-08-31 13:03:08 -0700

    Add: binary-tree-diameter - submission-11

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-11.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    int dia = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        helper(root);
+        return dia;
+    }
+
+    int helper(TreeNode root){
+        if(root == null) return 0;
+        int lh = helper(root.left);
+        int rh = helper(root.right);
+        dia = Math.max(dia, lh + rh);
+        return 1 + Math.max(lh, rh);
+    }
+}
