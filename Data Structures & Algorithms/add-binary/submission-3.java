03808c683274ad5dda884d39463788d115ece213
Aditya Rallapalli <124700496+Raditya0902@users.noreply.github.com>
2026-09-01 09:36:28 -0700

    Add: add-binary - submission-3

---
+++ Data Structures & Algorithms/add-binary/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1, j = b.length() - 1;
+        int carry = 0;
+
+        while(i >= 0 || j >= 0 || carry != 0){
+            int sum = carry;
+            sum += (i >= 0) ? a.charAt(i--) - '0' : 0;
+            sum += (j >= 0) ? b.charAt(j--) - '0' : 0;
+            sb.append(sum%2);
+            carry = sum/2;
+        }
+
+        return sb.reverse().toString();
+    }
+}
