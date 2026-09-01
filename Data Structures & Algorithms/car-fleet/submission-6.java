d813234f5cec2fa3fd15713f97a570eaa2c6a90e
Aditya Rallapalli <124700496+Raditya0902@users.noreply.github.com>
2026-08-31 22:01:41 -0700

    Add: car-fleet - submission-6

---
+++ Data Structures & Algorithms/car-fleet/submission-6.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int[][] pairs = new int[speed.length][2];
+        for(int i = 0; i < pairs.length; i++){
+            pairs[i][0] = position[i];
+            pairs[i][1] = speed[i];
+        }
+        Arrays.sort(pairs, (a,b)-> b[0] - a[0]);
+
+        Stack<Double> st = new Stack<>();
+        for(int[] p: pairs){
+            st.push((target - p[0])/(double)p[1]);
+            while(st.size() >= 2 && st.peek() <= st.get(st.size() - 2)) st.pop();
+
+        }
+
+        return st.size();
+    }
+    
+}
